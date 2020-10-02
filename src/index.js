1a9e41329944b70c20288ee9a0cdfc63d25d6e57
Eduard-D2002 <edomalchuk@gmail.com>
2020-10-02 23:08:29 +0300

    feat:implement min max avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(arguments.length == 0||array.length == 0){
+    return 0;
+ }
+ return Math.min.apply(null, array)
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(arguments.length == 0||array.length == 0){
+    return 0;
+ }
+ return Math.max.apply(null, array)
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(arguments.length == 0||array.length == 0){
+    return 0;
+ }
+ let total = 0;
+ for(let i = 0; i < array.length; i++) {
+   total += array[i];
+ }
+ return total / array.length
 }
